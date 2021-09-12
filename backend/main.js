8d1931d9eed61de2ec75f7422b26c8e7865a88f6
Sasha Le <iamsasha.le@gmail.com>
2021-09-12 23:05:55 +1000

    Initial commit

---
+++ backend/main.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+const express =require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3001);
